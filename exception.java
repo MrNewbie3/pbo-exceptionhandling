45a20eec45d56132a96d39f18ee478368d0c5b9f
Mr.Newbie <atharafiaffandy@gmail.com>
2022-05-17 09:53:55 +0700

    latihan 1 2 3 4

---
+++ exception.java
@@ -0,0 +1,25 @@
+package exception;
+
+public class exception {
+     static class AgeInvalid extends Exception{
+        AgeInvalid(String s){
+            super (s);
+        }
+    }
+         static void validate(int age)throws AgeInvalid{
+            if(age<18)
+                throw new AgeInvalid("not valid");
+            else
+                System.out.println("welcome to vote");
+        }
+        public static void main(String args[]){
+            try {
+                validate(13);
+            }
+            catch(Exception i){
+                System.out.println("Exception aktif " + i);
+            }
+            System.out.println("rest ");
+        }
+    }
+
